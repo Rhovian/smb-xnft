bd94e26d171f841b1412ec40d46d831311f87ab9
k <mja>
2023-05-18 22:58:15 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/dev";
-
-import { ExamplesScreens } from "./screens/ExamplesScreen";
 import { HomeScreen } from "./screens/HomeScreen";
-import { TokenListNavigator } from "./screens/TokenNavigator";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,37 +15,16 @@ function TabNavigator() {
       initialRouteName="Home"
       screenOptions={{
         tabBarActiveTintColor: "#e91e63",
+        headerShown: false,
       }}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="List"
-        component={TokenListNavigator}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Tokens",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bank" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Examples"
-        component={ExamplesScreens}
-        options={{
-          tabBarLabel: "Examples",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
+          tabBarButton: () => null,
+          headerTitle: "",
+          tabBarStyle: {display: "none"}
         }}
       />
     </Tab.Navigator>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,31 +1,27 @@
-import { Text, FlatList } from "react-native";
-import tw from "twrnc";
-
+import { StyleSheet } from "react-native";
 import { Screen } from "../components/Screen";
 
 export function HomeScreen() {
-  const features = [
-    "tailwind",
-    "recoil",
-    "native styling",
-    "fetching code from an API",
-    "using a FlatList to render data",
-    "Image for both remote & local images",
-    "custom fonts",
-    "sign a transaction / message",
-    "theme hook with light/dark support",
-  ];
+  const source = require('../assets/banana_split_066.pdf')
+
 
   return (
-    <Screen>
-      <Text style={tw`mb-4`}>
-        You'll find several examples of how to build xNFTs using react-native:
-      </Text>
-      <FlatList
-        data={features}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <Text>- {item}</Text>}
-      />
+    <Screen style={styles.container}>
+      <iframe style={styles.iframe} src="https://drive.google.com/file/d/1rFDNjF2tF7Zjq9y_lKTYcfcPmEUA61gu/preview" allow="autoplay"></iframe>
     </Screen>
   );
 }
+
+const styles  = StyleSheet.create(
+  {
+    container: {
+      height: '100vh',
+      padding: 0,
+    },
+    iframe: {
+      aspectRatio: 16/9,
+      width: '100%',
+      height: '100%',
+    }
+  }
+);
