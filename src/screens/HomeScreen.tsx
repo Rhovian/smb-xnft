83861e99c38034f91063efa9f6ea3aeaa8da552b
0xDon <22967202+thedonmon@users.noreply.github.com>
2023-05-21 17:38:52 -0400

    add loading screen and url fetch

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,33 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ActivityIndicator, View, Dimensions } from "react-native";
 import { Screen } from "../components/Screen";
+import { useLatestUrl } from "../hooks/useLatestUrl";
 
 export function HomeScreen() {
+  const data = useLatestUrl();
+  const { height, width } = Dimensions.get('window');
   return (
-    <Screen style={styles.container}>
-      <iframe style={styles.iframe} src="https://drive.google.com/file/d/1rFDNjF2tF7Zjq9y_lKTYcfcPmEUA61gu/preview" allow="autoplay"></iframe>
-    </Screen>
+    <>
+      {data.isLoading ?
+        <View
+          style={{ justifyContent: 'center', alignItems: 'center', height, width }}>
+          <ActivityIndicator size="large" color="black" />
+        </View> : (
+          <Screen style={styles.container}>
+            <iframe style={styles.iframe} src={data.data?.url} allow="autoplay"></iframe>
+          </Screen>
+        )}
+    </>
   );
 }
 
-const styles  = StyleSheet.create(
+const styles = StyleSheet.create(
   {
     container: {
       height: '100vh',
       padding: 0,
     },
     iframe: {
-      aspectRatio: 16/9,
+      aspectRatio: 16 / 9,
       width: '100%',
       height: '100%',
     }
